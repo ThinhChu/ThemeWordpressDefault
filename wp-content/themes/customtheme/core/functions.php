8b3d650c6c013dc032c4c26e437f907c473908b0
ThinhChuQuang <chuquangthinh34@gmail.com>
2024-03-05 09:30:03 +0700

    Update Theme 5/3 v3

--- wp-content/themes/customtheme/core/functions.php
+++ wp-content/themes/customtheme/core/functions.php
@@ -5,5 +5,58 @@
         }
         return $html;
     }
-    
+
     add_filter('get_custom_logo', 'add_custom_logo_class');
+
+    /**
+     * Undocumented function
+     * kiếm trang xem string/array đó có rỗng hay không
+     * @param array $content_args
+     * @return boolean
+     */
+    function content_exists( $content_args = [] ) {
+        if ( ! empty ( $content_args ) ) {
+            $done  = 0;
+            $total = count( $content_args );
+            foreach ( $content_args as $key => $value ) {
+                if ( ! is_array( $value ) && $value != '' || is_array( $value ) && content_exists( $value ) ) {
+                    $done++;
+                }
+            }
+            if ( isset ( $done ) && $done > 0 ) {
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+    }
+
+    /**
+     * Undocumented function
+     * Trả về format phone
+     * @param [type] $hotline
+     * @return void
+     */
+    function replace_tel( $hotline = '' ) {
+        if ( empty ( $hotline ) ) {
+            return;
+        }
+        $string   = preg_replace( '/\s+/','',$hotline );
+        $stringaz = preg_replace( '/[^a-zA-Z0-9_ -]/s', '', $string );
+        $tel = 'tel:'.$stringaz;
+        return $tel;
+
+    }
+
+    function get_href_by_type_social($type, $content) {
+        if ($type == 'phone') {
+            $href = replace_tel($content);
+        }elseif ($type == 'email') {
+            $href = 'mailto:'.$content;
+        }else{
+            $href = 'javascript:;';
+        }
+        return $href;
+    }
